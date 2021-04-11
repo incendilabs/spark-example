6392c88d0de5fe1d2d3ef3fb2d0ec54318dab844
Kenneth Myhra <kennethmyhra@gmail.com>
2021-04-11 19:09:48 +0200

    dist: Spark 1.5.3 packages and use IAsyncFhirService
    * Upgraded to Spark 1.5.3 packages and using IAsyncFhirService. Also
      moved the FhirController methods to async pattern

--- Controllers/FhirController.cs
+++ Controllers/FhirController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
@@ -10,33 +11,33 @@ using Microsoft.AspNetCore.Mvc;
 using Spark.Engine;
 using Spark.Engine.Core;
 using Spark.Engine.Extensions;
+using Spark.Engine.Service;
 using Spark.Engine.Utility;
-using Spark.Service;
 
 namespace SparkFhirExample.Controllers
 {
     [ApiController]
     public class FhirController : ControllerBase
     {
-        private readonly IFhirService _fhirService;
+        private readonly IAsyncFhirService _fhirService;
         private readonly SparkSettings _settings;
 
-        public FhirController(IFhirService fhirService, SparkSettings settings)
+        public FhirController(IAsyncFhirService fhirService, SparkSettings settings)
         {
             _fhirService = fhirService ?? throw new ArgumentNullException(nameof(fhirService));
             _settings = settings ?? throw new ArgumentNullException(nameof(settings));
         }
 
         [HttpGet("{type}/{id}")]
-        public ActionResult<FhirResponse> Read(string type, string id)
+        public async Task<FhirResponse> Read(string type, string id)
         {
             ConditionalHeaderParameters parameters = new ConditionalHeaderParameters(Request);
             Key key = Key.Create(type, id);
-            return new ActionResult<FhirResponse>(_fhirService.Read(key, parameters));
+            return await _fhirService.ReadAsync(key, parameters);
         }
 
         [HttpPut("{type}/{id?}")]
-        public ActionResult<FhirResponse> Update(string type, Resource resource, string id = null)
+        public async Task<FhirResponse> Update(string type, Resource resource, string id = null)
         {
             string versionId = Request.GetTypedHeaders().IfMatch?.FirstOrDefault()?.Tag.Buffer;
             Key key = Key.Create(type, id, versionId);
@@ -44,17 +45,17 @@ namespace SparkFhirExample.Controllers
             {
                 Request.TransferResourceIdIfRawBinary(resource, id);
 
-                return new ActionResult<FhirResponse>(_fhirService.Update(key, resource));
+                return await _fhirService.UpdateAsync(key, resource);
             }
             else
             {
-                return new ActionResult<FhirResponse>(_fhirService.ConditionalUpdate(key, resource,
-                    SearchParams.FromUriParamList(Request.TupledParameters())));
+                return await _fhirService.ConditionalUpdateAsync(key, resource,
+                    SearchParams.FromUriParamList(Request.TupledParameters()));
             }
         }
 
         [HttpPost("{type}")]
-        public FhirResponse Create(string type, Resource resource)
+        public async Task<FhirResponse> Create(string type, Resource resource)
         {
             Key key = Key.Create(type, resource?.Id);
 
@@ -65,29 +66,27 @@ namespace SparkFhirExample.Controllers
                     searchQueryString.Keys.Cast<string>()
                         .Select(k => new Tuple<string, string>(k, searchQueryString[k]));
 
-                return _fhirService.ConditionalCreate(key, resource, SearchParams.FromUriParamList(searchValues));
+                return await _fhirService.ConditionalCreateAsync(key, resource, SearchParams.FromUriParamList(searchValues));
             }
 
-            return _fhirService.Create(key, resource);
+            return await _fhirService.CreateAsync(key, resource);
         }
 
         [HttpDelete("{type}/{id}")]
-        public FhirResponse Delete(string type, string id)
+        public async Task<FhirResponse> Delete(string type, string id)
         {
             Key key = Key.Create(type, id);
-            FhirResponse response = _fhirService.Delete(key);
+            FhirResponse response = await _fhirService.DeleteAsync(key);
             return response;
         }
 
         [HttpGet("{type}")]
-        public FhirResponse Search(string type)
+        public async Task<FhirResponse> Search(string type)
         {
             int start = FhirParameterParser.ParseIntParameter(Request.GetParameter(FhirParameter.SNAPSHOT_INDEX)) ?? 0;
             var searchparams = Request.GetSearchParams();
-            //int pagesize = Request.GetIntParameter(FhirParameter.COUNT) ?? Const.DEFAULT_PAGE_SIZE;
-            //string sortby = Request.GetParameter(FhirParameter.SORT);
 
-            return _fhirService.Search(type, searchparams, start);
+            return await _fhirService.SearchAsync(type, searchparams, start);
         }
     }
 }
