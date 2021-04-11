7ae49dab877796200aa9db29721f34c4f00fbdc3
Kenneth Myhra <kennethmyhra@gmail.com>
2021-04-11 19:00:41 +0200

    fix: Ignore favicon.ico requests

---
+++ IgnoreRouteMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace spark_example
+{
+    public class IgnoreRouteMiddleware
+    {
+
+        private readonly RequestDelegate next;
+
+        // You can inject a dependency here that gives you access
+        // to your ignored route configuration.
+        public IgnoreRouteMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.HasValue &&
+                context.Request.Path.Value.Contains("favicon.ico"))
+            {
+
+                context.Response.StatusCode = 404;
+
+                return;
+            }
+
+            await next.Invoke(context);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -42,6 +42,7 @@ namespace spark_example
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<IgnoreRouteMiddleware>();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
