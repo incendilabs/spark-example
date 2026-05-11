6f8af2eb0c78b40f4526c09010931da60460eaf0
Kenneth Myhra <kennethmyhra@gmail.com>
2026-05-11 19:50:10 +0200

    Move to file-scoped namespace

--- Controllers/FhirController.cs
+++ Controllers/FhirController.cs
@@ -20,79 +20,86 @@ using Spark.Engine.Extensions;
 using Spark.Engine.Service;
 using Spark.Engine.Utility;
 
-namespace SparkFhirExample.Controllers
+namespace SparkFhirExample.Controllers;
+
+[ApiController]
+public class FhirController : ControllerBase
 {
-    [ApiController]
-    public class FhirController : ControllerBase
+    private readonly IFhirService _fhirService;
+    private readonly SparkSettings _settings;
+
+    public FhirController(IFhirService fhirService, SparkSettings settings)
     {
-        private readonly IFhirService _fhirService;
-        private readonly SparkSettings _settings;
+        _fhirService = fhirService ?? throw new ArgumentNullException(nameof(fhirService));
+        _settings = settings ?? throw new ArgumentNullException(nameof(settings));
+    }
 
-        public FhirController(IFhirService fhirService, SparkSettings settings)
+    [HttpGet("{type}/{id}")]
+    public async Task<FhirResponse> Read(string type, string id)
+    {
+        ConditionalHeaderParameters parameters = new ConditionalHeaderParameters(Request);
+        Key key = Key.Create(type, id);
+        return await _fhirService.ReadAsync(key, parameters);
+    }
+
+    [HttpPut("{type}/{id?}")]
+    public async Task<FhirResponse> Update(string type, Resource resource, string id = null)
+    {
+        string versionId = Request.GetTypedHeaders().IfMatch?.FirstOrDefault()?.Tag.Buffer;
+        Key key = Key.Create(type, id, versionId);
+        if (key.HasResourceId())
         {
-            _fhirService = fhirService ?? throw new ArgumentNullException(nameof(fhirService));
-            _settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            Request.TransferResourceIdIfRawBinary(resource, id);
+
+            return await _fhirService.UpdateAsync(key, resource);
+        }
+        else
+        {
+            return await _fhirService.ConditionalUpdateAsync(
+                key,
+                resource,
+                SearchParams.FromUriParamList(Request.TupledParameters())
+            );
+        }
+    }
+
+    [HttpPost("{type}")]
+    public async Task<FhirResponse> Create(string type, Resource resource)
+    {
+        Key key = Key.Create(type, resource?.Id);
+
+        if (Request.Headers.ContainsKey(FhirHttpHeaders.IfNoneExist))
+        {
+            NameValueCollection searchQueryString =
+                HttpUtility.ParseQueryString(Request.GetTypedHeaders().IfNoneExist());
+            IEnumerable<Tuple<string, string>> searchValues =
+                searchQueryString.Keys.Cast<string>()
+                    .Select(k => new Tuple<string, string>(k, searchQueryString[k]));
+
+            return await _fhirService.ConditionalCreateAsync(
+                key,
+                resource,
+                SearchParams.FromUriParamList(searchValues)
+            );
         }
 
-        [HttpGet("{type}/{id}")]
-        public async Task<FhirResponse> Read(string type, string id)
-        {
-            ConditionalHeaderParameters parameters = new ConditionalHeaderParameters(Request);
-            Key key = Key.Create(type, id);
-            return await _fhirService.ReadAsync(key, parameters);
-        }
+        return await _fhirService.CreateAsync(key, resource);
+    }
 
-        [HttpPut("{type}/{id?}")]
-        public async Task<FhirResponse> Update(string type, Resource resource, string id = null)
-        {
-            string versionId = Request.GetTypedHeaders().IfMatch?.FirstOrDefault()?.Tag.Buffer;
-            Key key = Key.Create(type, id, versionId);
-            if(key.HasResourceId())
-            {
-                Request.TransferResourceIdIfRawBinary(resource, id);
+    [HttpDelete("{type}/{id}")]
+    public async Task<FhirResponse> Delete(string type, string id)
+    {
+        Key key = Key.Create(type, id);
+        FhirResponse response = await _fhirService.DeleteAsync(key);
+        return response;
+    }
 
-                return await _fhirService.UpdateAsync(key, resource);
-            }
-            else
-            {
-                return await _fhirService.ConditionalUpdateAsync(key, resource,
-                    SearchParams.FromUriParamList(Request.TupledParameters()));
-            }
-        }
+    [HttpGet("{type}")]
+    public async Task<FhirResponse> Search(string type)
+    {
+        int start = FhirParameterParser.ParseIntParameter(Request.GetParameter(FhirParameter.SNAPSHOT_INDEX)) ?? 0;
+        var searchparams = Request.GetSearchParams();
 
-        [HttpPost("{type}")]
-        public async Task<FhirResponse> Create(string type, Resource resource)
-        {
-            Key key = Key.Create(type, resource?.Id);
-
-            if (Request.Headers.ContainsKey(FhirHttpHeaders.IfNoneExist))
-            {
-                NameValueCollection searchQueryString = HttpUtility.ParseQueryString(Request.GetTypedHeaders().IfNoneExist());
-                IEnumerable<Tuple<string, string>> searchValues =
-                    searchQueryString.Keys.Cast<string>()
-                        .Select(k => new Tuple<string, string>(k, searchQueryString[k]));
-
-                return await _fhirService.ConditionalCreateAsync(key, resource, SearchParams.FromUriParamList(searchValues));
-            }
-
-            return await _fhirService.CreateAsync(key, resource);
-        }
-
-        [HttpDelete("{type}/{id}")]
-        public async Task<FhirResponse> Delete(string type, string id)
-        {
-            Key key = Key.Create(type, id);
-            FhirResponse response = await _fhirService.DeleteAsync(key);
-            return response;
-        }
-
-        [HttpGet("{type}")]
-        public async Task<FhirResponse> Search(string type)
-        {
-            int start = FhirParameterParser.ParseIntParameter(Request.GetParameter(FhirParameter.SNAPSHOT_INDEX)) ?? 0;
-            var searchparams = Request.GetSearchParams();
-
-            return await _fhirService.SearchAsync(type, searchparams, start);
-        }
+        return await _fhirService.SearchAsync(type, searchparams, start);
     }
 }
